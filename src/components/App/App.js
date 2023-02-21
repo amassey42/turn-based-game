63ab83f186e166cf81272097230e81b29b3227f9
andrew <ammassey42@gmail.com>
2023-02-21 08:29:56 -0800

    start menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,19 @@
-import styles from './styles.module.css'
+import { StartMenu } from 'components/StartMenu';
+import { useState } from 'react';
+import styles from './styles.module.css';
 
-export const App =() => {
-  return (<div className={styles.main}>App</div>);
+export const App = () => {
+  const [mode, setMode] = useState('start');
+
+  return (
+    <div className={styles.main}>
+      {mode === 'start' && (
+        <StartMenu onStartClick={() => setMode('battle')} />
+      )}
+
+      {mode === 'battle' && <>Battle Mode</>}
+
+      {mode === 'gameOver' && <>Game Over</>}
+    </div>
+  );
 };
-
-
